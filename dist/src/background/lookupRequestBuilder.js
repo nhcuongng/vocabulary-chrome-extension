878b12449dc9a8625beb9d23c2d5608a594e4382
Cuong Nguyen Huu <cuongnh@fint.vn>
2026-03-28 23:02:02 +0700

    fix: delete dist

--- dist/src/background/lookupRequestBuilder.js
+++
@@ -1,9 +0,0 @@
-const DICTIONARY_BASE_URL = 'https://www.vocabulary.com/dictionary/';
-
-export function buildDictionaryLookupUrl(normalizedHeadword) {
-  if (!/^[a-z]+(?:[\-'][a-z]+)*$/.test(normalizedHeadword ?? '')) {
-    throw new Error('normalizedHeadword is invalid');
-  }
-
-  return new URL(encodeURIComponent(normalizedHeadword), DICTIONARY_BASE_URL).toString();
-}
